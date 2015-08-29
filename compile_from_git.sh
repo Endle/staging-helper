7d563034e5e2a8719cc2125f8f6603c79af7a651
Zhenbo Li <litimetal@gmail.com>
2015-08-29 20:33:32 +0800

    Only hack for old autoconf when necessary

--- compile_from_git.sh
+++ compile_from_git.sh
@@ -14,10 +14,14 @@ else
     cd code
 fi
 
-patch -p1 < ../centos_hack.diff
-echo "Applying wine-staging patches from"$STAGING to $(pwd)
+# Hack for old autoconf
+export AUTOCONF_VERSION=`autoconf --version|head -n 1|cut -d " " -f 4`
+if [ $AUTOCONF_VERSION \< 2.69 ]; then
+    patch -p1 < ../centos_hack.diff
+    echo "Applying wine-staging patches from"$STAGING to $(pwd)
+fi
 
-$STAGING/patches/patchinstall.sh DESTDIR="$(pwd)" --all
+$STAGING/patches/patchinstall.sh DESTDIR="$(pwd)" --all --force-autoconf
 ./configure --with-xattr --prefix=$PREFIX CC="ccache gcc" CFLAGS="-g -O0 -m32"
 make -j4
 #make install
