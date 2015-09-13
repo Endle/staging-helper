86a283d4455b101845522973398cb8df626c80d1
Zhenbo Li <litimetal@gmail.com>
2015-09-13 23:17:11 +0800

    More configure options

--- compile_from_git.sh
+++ compile_from_git.sh
@@ -5,6 +5,58 @@ source ./config.sh
 
 export WORKING_DIR=$(pwd)
 
+FETCH_WINE=0
+FETCH_STAGE=0
+NO_CONFIGURE=0
+NO_MAKE=0
+MAKE_OPTS="-j4" #FIXME: only support -Jn
+
+for i in "$@"
+do
+    case $i in
+    --fetch_wine)
+        FETCH_WINE=1
+        shift
+        ;;
+    --fetch_stage)
+        FETCH_STAGE=1
+        shift
+        ;;
+    -s|--simple)
+        FETCH_STAGE=1
+        FETCH_WINE=1
+        shift
+        ;;
+    --no_make)
+        NO_MAKE=1
+        shift
+        ;;
+    --no_configure)
+        NO_CONFIGURE=1
+        shift
+        ;;
+    -j*)
+        MAKE_OPTS=$1
+        shift
+        ;;
+    *)
+        ;;
+esac
+done
+
+# FIXME: 考虑的情况太简单
+if [ $FETCH_WINE \= 1 ]; then
+    echo "Fetch Wine"
+    cd $WINEGIT
+    git pull origin master
+    cd $WORKING_DIR
+fi
+if [ $FETCH_STAGE \= 1 ]; then
+    echo "Fetch Wine-Stage" cd $STAGING
+    git pull origin master --force
+    cd $WORKING_DIR
+fi
+
 echo "Copying wine-git code from"$WINEGIT
 if [ -d "$WINE_PATCHED" ]; then
     cd $WINE_PATCHED
@@ -23,7 +75,12 @@ if [ $AUTOCONF_VERSION \< 2.69 ]; then
     echo "Applying wine-staging patches from"$STAGING to $(pwd)
 fi
 
-#$STAGING/patches/patchinstall.sh DESTDIR="$(pwd)" --all --force-autoconf --backend=git-am
-#./configure --with-xattr --prefix=$PREFIX CC="ccache gcc" CFLAGS="-g -O0 -m32"
-#make -j4
-#make install
+$STAGING/patches/patchinstall.sh DESTDIR="$(pwd)" --all --force-autoconf --backend=git-am
+if [ $NO_CONFIGURE \= 0 ]; then
+    ./configure --with-xattr --prefix=$PREFIX CC="ccache gcc" CFLAGS="-g -O0 -m32"
+    if [ $NO_MAKE \= 0 ]; then
+        make $MAKE_OPTS
+        #FIXME: not supported yet
+        #make install
+    fi
+fi
